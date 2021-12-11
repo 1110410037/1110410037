d6e2d4c93798a8b8d0dd6f1ceb7f72005e25d7f5
1110410037 <d10410037@ems.npu.edu.tw>
2021-12-11 20:08:44 +0800

    加入專案檔案。

---
+++ 三角形-1/三角形-1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    for(int a=1; a <= x; a++) {
+        for(int b=0; b < a; b++)cout << "*";
+        cout << "\n";
+    }
+
+    int y;
+    cin >> y;
+    for(int a=y; a >= 1; a--) {
+        for(int b=0; b < a; b++)cout << "*";
+        cout << "\n";
+    }
+
+    int z;
+    cin >> z;
+    for(int a=1; a <= z;a++){
+        for(int b=0; b < z-a; b++)cout << " ";
+        for(int c=0; c < a; c++)cout << "*";
+        cout<<"\n";
+    }
+
+    int i;
+    cin >> i;
+    for(int a=i; a >= 1; a--) {
+        for(int b=0; b < i-a; b++)cout << " ";
+        for(int c=0; c < a; c++)cout << "*";
+        cout << "\n";
+    }
+}
